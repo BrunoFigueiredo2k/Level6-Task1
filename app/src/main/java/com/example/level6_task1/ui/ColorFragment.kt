5d6dae12e2419d517542d776c4094cd16463bad0
@BrunoFigueiredo2k <brunofigueiredo@live.nl>
2020-11-14 14:10:27 +0100

    init project and dependencies

---
+++ app/src/main/java/com/example/level6_task1/ui/ColorFragment.kt
@@ -0,0 +1,23 @@
+package com.example.level6_task1.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.level6_task1.R
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class ColorFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_color, container, false)
+    }
+
+}
